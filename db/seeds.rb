1c5cf18febfcb859a9989847f5277f4284f7c9e4
Arvin Fernandez <arvinf07@gmail.com>
2020-12-21 11:52:56 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.new(first_name: "Arvin", last_name: "Fernandez")
+Student.new(first_name: "Xena", last_name: "Taveras")
